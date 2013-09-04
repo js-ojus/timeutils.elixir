0635c15e2277a6836f4655faa33ee922bec12a37
JONNALAGADDA Srinivas <js@ojuslabs.com>
2013-09-04 15:57:28 +0530

    Added a few tests.

--- test/time_utils_test.exs
+++ test/time_utils_test.exs
@@ -1,7 +1,40 @@
 defmodule TimeUtilsTest do
   use ExUnit.Case
+  import TimeUtils
 
-  test "the truth" do
-    assert(true)
+  test "seconds before" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 15 |> seconds |> before tnow
+    assert {{2013, 9, 4}, {10, 37, 30}} == t
+  end
+
+  test "seconds from" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 15 |> seconds |> from tnow
+    assert {{2013, 9, 4}, {10, 38, 0}} == t
+  end
+
+  test "minutes before" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 25 |> minutes |> before tnow
+    assert {{2013, 9, 4}, {10, 12, 45}} == t
+  end
+
+  test "minutes from" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 25 |> minutes |> from tnow
+    assert {{2013, 9, 4}, {11, 2, 45}} == t
+  end
+
+  test "hours before" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 11 |> hours |> before tnow
+    assert {{2013, 9, 3}, {23, 37, 45}} == t
+  end
+
+  test "hours from" do
+    tnow = {{2013, 9, 4}, {10, 37, 45}}
+    t = 14 |> hours |> from tnow
+    assert {{2013, 9, 5}, {0, 37, 45}} == t
   end
 end
